d46aa1b1ed56f09b77baeaf7c0bff6a4ea1feda5
Mefdev <mefdev.pro@gmail.com>
2024-03-10 15:48:14 +0000

    pascal remenisente

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -29,10 +29,10 @@ def pascal_triangle(numRows):
 
 def findFact(n):
     """Find the factorial of a number"""
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * findFact(n - 1)
 
 
 def findComb(n, k):
